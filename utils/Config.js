c1f4d183d3fcaf7e0e2b63dc0846303e27c26791
Ghosth <ghleomyre@gmail.com>
2025-02-08 15:23:01 +0300

    .

--- utils/Config.js
+++ utils/Config.js
@@ -1,24 +1,3 @@
-/* const UrlConfig = {
-    apiBaseUrl: 'http://127.0.0.1:8000',
-    // apiBaseUrl: 'http://192.168.88.57:8000',
-    adminUrl: '/admin',
-};
-
-const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
-
-
-export default UrlConfig;
-
-export { UrlConfig, firebaseConfig  };
- */
 
 export const UrlConfig = {
     apiBaseUrl: 'http://127.0.0.1:8000',
